39d97c732d24f5f34a3ee6f6255c84783dff3039
Darwin Correa <dacopanbsc@gmail.com>
2015-08-11 12:13:44 -0500

    fix dockbar popup z-index

--- public_html/perseo/my_html/js/dcm_common.js
+++ public_html/perseo/my_html/js/dcm_common.js
@@ -2282,38 +2282,42 @@ var DockPopUp = (function () {
             });
 
             $close.on('click', function () {
+
                 $body.css('overflow-y', 'auto');
+
                 var layoutProp = getItemLayoutProp($item),
-                        clipPropFirst = 'rect(' + layoutProp.top + 'px ' + (layoutProp.left + layoutProp.width) + 'px ' + (layoutProp.top + layoutProp.height) + 'px ' + layoutProp.left + 'px)',
-                        clipPropLast = 'auto';
+					clipPropFirst = 'rect(' + layoutProp.top + 'px ' + (layoutProp.left + layoutProp.width) + 'px ' + (layoutProp.top + layoutProp.height) + 'px ' + layoutProp.left + 'px)',
+					clipPropLast = 'auto';
+
                 // reset current
                 current = -1;
+
                 $overlay.css({
                     clip: supportTransitions ? clipPropFirst : clipPropLast,
                     opacity: supportTransitions ? 1 : 0,
                     pointerEvents: 'none'
                 });
+
                 if (supportTransitions) {
                     $overlay.on(transEndEventName, function () {
 
-                        //$overlay.off(transEndEventName);
+                        $overlay.off(transEndEventName);
                         setTimeout(function () {
                             $overlay.css('opacity', 0).on(transEndEventName, function () {
                                 $overlay.off(transEndEventName).css({ clip: clipPropLast, zIndex: -1 });
                                 $item.data('isExpanded', false);
-                                $('#dock').trigger("click");
                             });
                         }, 25);
+
                     });
                 }
                 else {
                     $overlay.css('z-index', -1);
                     $item.data('isExpanded', false);
-                    $('#dock').trigger("click");
                 }
 
-
                 return false;
+
             });
         });
         $(window).on('debouncedresize', function () {
